630deadb20bddf8a3fb56ece9850f3f41cd3e7b0
abh-pro <abhishek.chebolu@gmail.com>
2021-01-23 14:12:18 +0530

    script change

--- script.sh
+++ script.sh
@@ -1,6 +1,4 @@
 #checking if cuda compiler is available
 which nvcc 1>/dev/null 2>/dev/null; if [ $? -gt 0 ]; then echo "nvcc compiler not found."; exit; fi
-#the colab should check for the cu files available in the colab present working directory.
-[[ -f hello_cuda.cu ]] && rm hello_cuda.cu; [[ -f max_value.cu  ]] && rm max_value.cu; [[ -f min_value.cu ]] && rm min_value.cu;
 #compiling of the files as per availability.
 [[ -f hello_cuda.cu ]] && nvcc -o hello_cuda hello_cuda.cu ;[[ -f max_value.cu ]] && nvcc -o max_value max_value.cu ; [[ -f min_value.cu ]] && nvcc -o min_value min_value.cu ;
