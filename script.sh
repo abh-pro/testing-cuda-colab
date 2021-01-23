6524760a02ef993b3ff536d7275320d8871cddf8
abh-pro <abhishek.chebolu@gmail.com>
2021-01-23 13:49:00 +0530

    script changes.

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@ which nvcc 1>/dev/null 2>/dev/null; if [ $? -gt 0 ]; then echo "nvcc compiler no
 #the colab should check for the cu files available in the colab present working directory.
 [[ -f hello_cuda.cu ]] && rm hello_cuda.cu; [[ -f max_value.cu  ]] && rm max_value.cu; [[ -f min_value.cu ]] && rm min_value.cu;
 #wget the file from the github repository 
-# wget <urls>
+wget https://raw.githubusercontent.com/abh-pro/testing-cuda-colab/main/hello_cuda.cu
 
 #compiling of the files as per availability.
 [[ -f hello_cuda.cu ]] && nvcc -o hello_cuda hello_cuda.cu ;[[ -f max_value.cu ]] && nvcc -o max_value max_value.cu ; [[ -f min_value.cu ]] && nvcc -o min_value min_value.cu ;
